28a3c3301aaa2c88eca1ea358588af19328b4fcd
joon-guthub <ehdfkd2000@naver.com>
2024-06-09 16:26:09 +0900

    crrentScene 결정 반영

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 (() => {
+  let yOffset = 0; // 현재 스크롤 위치
+  let prevScrollHeight = 0; // 현재 스크롤 위치보다 이전에 위치한 섹션들의 스크롤 높이값의 합
+  let currentScene = 0; // 현재 활성화된 씬
   const sceneInfo = [
     {
       // 0
@@ -42,12 +45,30 @@
     // 각 스크롤 섹션의 높이 세팅
     for (let i = 0; i < sceneInfo.length; i++) {
       sceneInfo[i].scrollheight = sceneInfo[i].heightNum * window.innerHeight;
+      prevScrollHeight += sceneInfo[i].scrollheight;
       sceneInfo[
         i
       ].objs.container.style.height = `${sceneInfo[i].scrollheight}px`;
     }
   };
+  const scrollLopp = () => {
+    prevScrollHeight = 0;
+    for (let i = 0; i < currentScene; i++) {
+      prevScrollHeight += sceneInfo[i].scrollheight;
+    }
+
+    if (yOffset > prevScrollHeight + sceneInfo[currentScene].scrollheight) {
+      currentScene++;
+    } else if (yOffset < prevScrollHeight && yOffset > 0) {
+      currentScene--;
+    }
+    console.log(currentScene);
+  };
+
   window.addEventListener("resize", setlayOut);
+  window.addEventListener("scroll", () => {
+    yOffset = window.scrollY;
+    scrollLopp();
+  });
   setlayOut();
-  console.log(sceneInfo);
 })();
